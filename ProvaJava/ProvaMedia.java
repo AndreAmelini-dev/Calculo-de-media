9fd6d369d5e414865399f56853883a6d5070ca78
Andre Amelini <andreamelini2006@gmail.com>
2025-05-11 23:21:12 -0300

    Update ProvaMediaAc2.java

--- ProvaJava/ProvaMedia.java
+++ ProvaJava/ProvaMedia.java
@@ -1,51 +1,46 @@
+/*Andre Amelini de Faria Ra:252207
+ * Arthur Marcolino da Silva Ra:252369
+ * Thiago Andrade - 252434
+ */
 import java.util.Scanner;
-
-public class ProvaMedia {
+public class Ac2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+         Scanner scanner = new Scanner(System.in);
+         int QtdAluno = 0;
+         String[] provas = new String[0];
+         System.out.println("Quantos alunos tem na sala:");
+         QtdAluno = scanner.nextInt();
+        System.out.println("Qual sera a nota minima das provas:");
+        int MediasMinima = scanner.nextInt();
 
-        double prova1 = 0.0;
-        double prova2;
-        double prova3;
-        double prova4;
-        double media;
-        double notaMinima;
+        System.out.println("Qual sera a media da ac1:");
+         MediasMinima = scanner.nextInt();
+           System.out.println("Qual sera a media da ac2:");
+         MediasMinima = scanner.nextInt();
+           System.out.println("Qual sera a media da aF:");
+         MediasMinima = scanner.nextInt();
+           System.out.println("Qual sera a media da aG:");
+         MediasMinima = scanner.nextInt();
 
+        for(int P = 0;P < provas.length; P++ ){
+            System.out.println("Qual o nome do Aluno:");
+            provas[P] = scanner.next();
 
+             System.out.println("Qual o nome a nota da Ac1:");
+              provas[P] = scanner.next();
 
+              System.out.println("Qual o nome a nota da Ac2:");
+              provas[P] = scanner.next();
 
-        System.out.println("Calcule a sua média");
+              System.out.println("Qual a nota da AG");
+              provas[P] = scanner.next();
 
-        System.out.println("Digite a nota minima:");
-        notaMinima = scanner.nextDouble();
-        
+              System.out.println("Qual a nota da AF");
+              provas[P] = scanner.next();
 
-        System.out.println("Digite a nota da AC1:");
-        prova1 = scanner.nextDouble();
-
-        System.out.println("Digite a nota da AC2:");
-        prova2 = scanner.nextDouble();
-
-        System.out.println("Digite a nota da AG:");
-        prova3 = scanner.nextDouble();
-
-        System.out.println("Digite a nota da AF:");
-        prova4 = scanner.nextDouble(); 
-
-        
-        
-        media = prova1 * 0.15 + prova2 * 0.30 + prova3 * 0.10 + prova4 * 0.45;
-
-        System.out.println("Sua média é: " + media);
-
-        if (notaMinima <= media) {
-            System.out.println("Aprovado!");
-            System.out.println("AC1: " + prova1);
-            System.out.println("AC2: " + prova2);
-            System.out.println("AG: " + prova3);
-            System.out.println("AF: " + prova4);
-        } else {
-            System.out.println("Reprovado!");
         }
+        System.out.println("---------------");
+        scanner.close();
     }
+    
 }
